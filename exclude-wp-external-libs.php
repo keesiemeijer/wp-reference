58152336775ed6133024090aeabcce528195fa02
Kees Meijer <keesie.meijer@gmail.com>
2018-11-13 12:57:17 +0100

    Exlude same files and directories as the wporg-developer theme

--- exclude-wp-external-libs.php
+++ exclude-wp-external-libs.php
@@ -12,77 +12,45 @@ add_filter( 'wp_parser_pre_import_file', 'wppeel_exclude_external_libs', 10, 2 )
 
 function wppeel_exclude_external_libs( $include, $file ) {
 
+	// Bail early if file is already being skipped.
+	if ( ! $include ) {
+		return $include;
+	}
+
 	if ( ! ( isset( $file['path'] ) && $file['path'] ) ) {
 		return $include;
 	}
 
-	// Files to exclude.
-	$exclude_files = array(
+	$exclude = array(
+		'wp-admin/css/',
+		'wp-admin/includes/class-ftp',
 		'wp-admin/includes/class-pclzip.php',
-		'wp-admin/includes/class-ftp-pure.php',
 		'wp-admin/includes/class-ftp-sockets.php',
 		'wp-admin/includes/class-ftp.php',
-		'wp-includes/class-snoopy.php',
-		'wp-includes/class-simplepie.php',
+		'wp-admin/js/',
+		'wp-content/',
+		'wp-includes/ID3/',
+		'wp-includes/IXR/',
+		'wp-includes/SimplePie/',
+		'wp-includes/Text/',
+		'wp-includes/certificates/',
 		'wp-includes/class-IXR.php',
+		'wp-includes/class-json.php',
 		'wp-includes/class-phpass.php',
 		'wp-includes/class-phpmailer.php',
-		'wp-includes/class-pop3.php',
-		'wp-includes/class-json.php',
+		'wp-includes/class-pop3.php ',
+		'wp-includes/class-simplepie.php',
 		'wp-includes/class-smtp.php',
+		'wp-includes/class-snoopy.php',
+		'wp-includes/js/',
 	);
 
-	// Directories to exclude.
-	$exclude_dirs = array(
-		'wp-includes/ID3/',
-		'wp-includes/Text/',
-		'wp-includes/SimplePie/',
-	);
-
-	// Files to include from wp-content.
-	$include_wp_content_files  = array(
-		'wp-content/plugins/hello.php',
-	);
-
-	// Directories to Include from wp-content.
-	$include_wp_content_dirs  = array(
-		'wp-content/themes/twentyfourteen/',
-		'wp-content/themes/twentythirteen/',
-		'wp-content/themes/twentytwelve/',
-		'wp-content/themes/twentyeleven/',
-		'wp-content/themes/twentyten/',
-	);
-
-
-	if ( in_array( $file['path'], $exclude_files ) ) {
-		return false;
-	}
-
-	foreach ( $exclude_dirs as $dir ) {
-		if ( 0 === strpos( $file['path'], $dir ) ) {
-			return false;
-		}
-	}
-
-	// Check if path starts with wp-content.
-	if ( 0 !== strpos( $file['path'], 'wp-content/' ) ) {
-		return $include;
-	}
-
-	$wp_content = array_merge( $include_wp_content_dirs, $include_wp_content_files );
-
-	$include = false;
-	foreach ( $wp_content as $dir ) {
-		// Check if file or directory is whitelisted.
-		if ( 0 === strpos( $file['path'], $dir ) || ( $file['path'] === $dir ) ) {
-			$include = true;
+	// Skip file if it matches anything in the list.
+	foreach ( $exclude as $skip ) {
+		if ( 0 === strpos( $file['path'], $skip ) ) {
+			$include = false;
 			break;
 		}
-	}
-
-	// Exclude directories and files from wp-content if they're not whitelisted.
-	if ( ! $include ) {
-		return false;
 	}
 
 	return $include;
